46c19fa1176935a4b68a0f9a1ba664b31e838c89
Luke Olsen <lukeaolsen@gmail.com>
2019-04-08 22:58:01 -0400

    Server now activates and connects to mongoDb.

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,21 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('Mongoose')
 const PORT = 4000;
+require('dotenv').config()
 
 app.use(cors());
 app.use(bodyParser.json());
+
+
+mongoose.connect(process.env.DB_ROUTE, {useNewUrlParser: true});
+const conn = mongoose.connection
+
+conn.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
+
 
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
