8fe2b9362405a653ed2c59f0b69811dacbf1a9e1
Luke Olsen <lukeaolsen@gmail.com>
2019-04-11 10:42:33 -0400

    Can now connect and add data to mongodb atlas.

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ const PORT = 4000;
 const dataRoutes = express.Router();
 const userRoutes = express.Router();
 const MongoClient = require('mongodb').MongoClient;
-const client = new MongoClient(process.env.DB_ROUTE, {useNewUrlParser: true})
 require('dotenv').config()
 
 app.use(cors());
@@ -16,101 +15,82 @@ app.use(bodyParser.json());
 
 let Data = require('./data.model');
 let User = require('./user.model');
+const client = new MongoClient(process.env.DB_ROUTE, {useNewUrlParser: true})
 
 client.connect(err => {
-    const collection = client.db('Task-Data').collection('User')
+    const db = client.db(process.env.DB_NAME)
+    const user = db.collection('User')
     console.log("connected to DB")
-})
 
 
-// mongoose.connect(process.env.DB_ROUTE, {
-//     useNewUrlParser: true,
-//     dbName: 'Task-Data'
-// });
-// const conn = mongoose.connection
-
-// conn.once('open', function() {
-//     console.log("MongoDB database connection established successfully");
-// })
-
-app.get('/', function(req, res) {
-    console.log("WELCOME!")
-    res.json({"type": "you did it"})
-})
-
-dataRoutes.route('/').get(function(req, res) {
-    console.log("initial working");
-    Data.find(function(err, data) {
-        if (err) {
-            console.log(err)
-        } else {
-            res.json(data);
-        }
+    app.get('/', function(req, res) {
+        console.log("WELCOME!")
+        res.json({"type": "you did it"})
     })
-});
-
-dataRoutes.route('/:id').get(function(req, res){
-    let id = req.params.id;
-    Data.projects[Data.projects[Data.projects.active]].todo.findById(id, function(err, data){
-        res.json(data)
+    
+    dataRoutes.route('/').get(function(req, res) {
+        console.log("initial working");
+        Data.find(function(err, data) {
+            if (err) {
+                console.log(err)
+            } else {
+                res.json(data);
+            }
+        })
+    });
+    
+    dataRoutes.route('/:id').get(function(req, res){
+        let id = req.params.id;
+        Data.projects[Data.projects[Data.projects.active]].todo.findById(id, function(err, data){
+            res.json(data)
+        })
+    });
+    
+    dataRoutes.route('/add').post(function(req, res) {
+        console.log(req.body)
+    
+        data.save()
+            .then(data => {
+                res.status(200).json({'data': 'data added successfully!'})
+            })
+            .catch(err => {
+                res.status(400).send('adding new data failed');
+            })
     })
-});
-
-dataRoutes.route('/add').post(function(req, res) {
-    console.log(req.body)
-
-    data.save()
-        .then(data => {
-            res.status(200).json({'data': 'data added successfully!'})
+    
+    userRoutes.route('/add').post(function(req, res) {
+    
+        console.log(req.body);
+        const {userName, userId} = req.body;
+        console.log(userName)
+        
+        user.save(req.body)
+            .then(data => {
+                res.status(200).json({'data': 'data added successfully!'})
+            })
+            .catch(err => {
+                res.status(400).send('adding new data failed');
+            })
+    })
+    
+    userRoutes.route('/').get(function(req, res) {
+        console.log("green weasels")
+        User.find((err, users) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.json(users)
+            }
         })
-        .catch(err => {
-            res.status(400).send('adding new data failed');
-        })
+    })
+    
+    app.use('/user', userRoutes);
+    
+    app.use('/data', dataRoutes);
+    
+    app.listen(PORT, function() {
+        console.log("Server is running on Port: " + PORT);
+    });
 })
 
-userRoutes.route('/add').post(function(req, res) {
 
-    client.connect(err => {
-        const collection = client.db('Task-Data').collection('User')
-        console.log(collection)
-
-
-
-
-    })
-    console.log(req.body);
-    const {userName, userId} = req.body;
-    console.log(userName)
-    console.log(userId)
-    let user = new User();
- 
-
-    user.userName = userName;
-    user.userId = userId;
-    user.save()
-        .then(data => {
-            res.status(200).json({'data': 'data added successfully!'})
-        })
-        .catch(err => {
-            res.status(400).send('adding new data failed');
-        })
-})
-
-userRoutes.route('/').get(function(req, res) {
-    console.log("green weasels")
-    User.find((err, users) => {
-        if (err) {
-            console.log(err)
-        } else {
-            res.json(users)
-        }
-    })
-})
-
-app.use('/user', userRoutes);
-
-app.use('/data', dataRoutes);
-
-app.listen(PORT, function() {
-    console.log("Server is running on Port: " + PORT);
-});
