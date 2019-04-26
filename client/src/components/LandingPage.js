0b7a2b66d72fbb698d470087242a803af265cd3f
Luke Olsen <lukeaolsen@gmail.com>
2019-04-26 15:47:38 -0400

    Completed some basic styling on landing page.

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { setGuest } from '../actions/index';
 import { connect } from "react-redux"; 
+import './LandingPage.css'
+import Button from '@material-ui/core/Button';
 
 
 const mapStateToProps = state => {
@@ -17,9 +19,20 @@ const mapDispatchToProps = dispatch => {
 const LandingPage = ({setGuest}) => {
 
     return (
-        <div>
-            <h1>Welcome to the app!</h1>
-            <a href="http://localhost:4000/auth/google"> Log in with google.</a>
+        <div className="welcomeBackground">
+            <div className="mainWelcomeCard">
+                <div className="welcomeTextArea animated animatedFadeInUp fadeInUp">
+                    <h1 className="welcomeTitle ">Welcome to a free task management board!</h1>
+                </div>
+                <div className="welcomeButtons animatedLonger animatedFadeInUp fadeInUp">
+                    <div>
+                       
+                    </div>
+                    <div>
+                        <Button variant="contained" color="primary"><a href="http://localhost:4000/auth/google"> Log in with google</a></Button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
